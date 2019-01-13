be87adeea8deb7a3ec88ce06b08a7346efabc7e0
Dimitar Andreev Dimitrov <dandreevd@gmail.com>
2019-01-13 11:46:57 +0200

    Added SDK. Added Samples.

---
+++ StoryComb/StoryComb.SDK/Samples.cs
@@ -0,0 +1,69 @@
+﻿namespace StoryComb.SDK
+{
+    public class Samples
+    {
+        public string[] Characters
+        {
+            get
+            {
+                return new string[]
+                {
+                    "Android on steroids",
+                    "Flailing android",
+                    "Pirate with missing legs",
+                    "Two legs",
+                    "Two-headed ogre with one hand",
+                    "Midget martial artist",
+                    "Swearing wizard",
+                    "Bald village whore",
+                    "Telekinetic nun",
+                    "Fat samurai",
+                    "Virgin Greek god",
+                };
+            }
+        }
+
+        public string[] Goals
+        {
+            get
+            {
+                return new string[]
+                {
+                    "Prevent asteroid from colliding with the Moon",
+                    "Gee inheritance",
+                    "Free a nation of gt a girlfriend",
+                    "Find lost ship",
+                    "Fulfill father's wish to see the dragon",
+                    "End poverty",
+                    "Afforest desert",
+                    "Collect all runes",
+                    "Discover cure for rare disease",
+                    "Steal the totem",
+                    "Prevent train crash",
+                    "Hijack president's car",
+                    "Build a moat",
+                    "Distribute massiviants",
+                };
+            }
+        }
+
+        public string[] Twists
+        {
+            get
+            {
+                return new string[]
+                {
+                    "Best friend has heart attack",
+                    "Receives a letter from distant relative",
+                    "Volcano erupts",
+                    "Breaks a leg",
+                    "Neighbor kills cat",
+                    "Aliens arrive",
+                    "Loses tools",
+                    "Gets diagnosed with disease",
+                    "Best friend steals money",
+                };
+            }
+        }
+    }
+}
