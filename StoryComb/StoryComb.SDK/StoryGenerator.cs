fe2a130fd2bc8ed7b826ab26e6c4014d728e7476
Dimitar Andreev Dimitrov <dandreevd@gmail.com>
2019-01-13 12:01:24 +0200

    Added StoryGenerator.

---
+++ StoryComb/StoryComb.SDK/StoryGenerator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace StoryComb.SDK
+{
+    public class StoryGenerator
+    {
+        private readonly IStoryContainer storyContainer;
+        private readonly Random random;
+
+        public StoryGenerator()
+        {
+            this.storyContainer = new SampleStoryContainer();
+            this.random = new Random();
+        }
+
+        public StoryGenerator(IStoryContainer storyContainer)
+        {
+            this.storyContainer = storyContainer ?? 
+                throw new ArgumentNullException(nameof(storyContainer));
+            this.random = new Random();
+        }
+
+        public string GenerateStory()
+        {
+            var character = this.storyContainer.Characters[
+                this.random.Next(0, this.storyContainer.Characters.Length)].ToUpper();
+            var goal = this.storyContainer.Goals[
+                this.random.Next(0, this.storyContainer.Goals.Length)].ToUpper();
+            var twist = this.storyContainer.Twists[
+              this.random.Next(0, this.storyContainer.Twists.Length)].ToUpper();
+
+            return $"{character} must {goal} but {twist}.";
+        }
+    }
+}
