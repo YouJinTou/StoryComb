b2fec976d0b847b30bf28465e451c55099e9d52c
Dimitar Andreev Dimitrov <dandreevd@gmail.com>
2019-01-13 12:04:11 +0200

    Added StoryComb.ConsoleRunner.

---
+++ StoryComb/StoryComb.ConsoleRunner/Runner.cs
@@ -0,0 +1,20 @@
+﻿using StoryComb.SDK;
+using System;
+
+namespace StoryComb.ConsoleRunner
+{
+    public class Runner
+    {
+        public static void Main(string[] args)
+        {
+            var storyGenerator = new StoryGenerator();
+
+            while (true)
+            {
+                Console.ReadLine();
+
+                Console.WriteLine(storyGenerator.GenerateStory());
+            }
+        }
+    }
+}
